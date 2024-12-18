e5870afd6cb6a4947aeb4c660a45471e0326711e
Lokee <lokeevuza@gmail.com>
2024-12-18 12:33:33 +0300

    # - Изменена сумма элементов

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     const int size{5};
     int array[] = {5, 1, 2, 6, 1};
     
-    cout << "Сумма элементов: " << sum(array, 5, 1, 2) << endl;
+    cout << "Сумма элементов: " << sum(array, 5, 0, 5) << endl;
 
     cout << "Максимальное чисел: " << max(2, 6) << "\n";
     cout << "Минимальное чисел: " << min(2, 6) << "\n";
